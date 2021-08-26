dc63046e4290f622dc53f544bb1b9b8da0771fac
1026847645@qq.com <1026847645@qq.com>
2021-08-26 22:39:26 +0800

    接入etcd

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module pay_center
 go 1.15
 
 require (
-	github.com/douyu/jupiter v0.3.2
+	github.com/douyu/jupiter v0.2.7
 	github.com/gin-gonic/gin v1.7.0
+	github.com/golang/protobuf v1.4.2
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381
+	google.golang.org/grpc v1.26.0
 )
--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"github.com/douyu/jupiter"
+	compound_registry "github.com/douyu/jupiter/pkg/registry/compound"
+	"github.com/douyu/jupiter/pkg/registry/etcdv3"
 	"github.com/douyu/jupiter/pkg/server/xgin"
+	"github.com/douyu/jupiter/pkg/server/xgrpc"
 	"github.com/douyu/jupiter/pkg/xlog"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 func main() {
 	eng := NewEngine()
+	eng.SetRegistry(compound_registry.New(
+		etcdv3.StdConfig("wh").Build(),
+	))
 	if err := eng.Run(); err != nil {
 		xlog.Panic(err.Error())
 	}
@@ -22,6 +30,7 @@ func NewEngine() *Engine {
 	eng := &Engine{}
 	if err := eng.Startup(
 		eng.serveHTTP,
+		eng.serveGRPC,
 	); err != nil {
 		xlog.Panic("startup", xlog.Any("err", err))
 	}
@@ -36,4 +45,22 @@ func (eng *Engine) serveHTTP() error {
 		ctx.JSON(200, "Hello Gin")
 	})
 	return eng.Serve(server)
+}
+
+func (eng *Engine) serveGRPC() error {
+	server := xgrpc.StdConfig("grpc").Build()
+	helloworld.RegisterGreeterServer(server.Server, &Greeter{
+		server: server,
+	})
+	return eng.Serve(server)
+}
+
+type Greeter struct {
+	server *xgrpc.Server
+}
+
+func (g Greeter) SayHello(context context.Context, request *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+	return &helloworld.HelloReply{
+		Message: "Hello Jupiter, I'm " + g.server.Address(),
+	}, nil
 }
